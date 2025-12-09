87f01a4c2e36e049961e2c9b1a2fdbf586b4894a
Fidget <fidgetbeeby@gmail.com>
2025-12-08 21:28:43 -0500

    Removed useded nginx/A.nix and ngnix/www.A.nix

--- nginx/A.nix
+++
@@ -1,9 +0,0 @@
-{}:
-{
-  enableACME = true;
-  addSSL = true;
-  locations."/" = {
-    proxyPass = "http://127.0.0.1:8096";
-    proxyWebsockets = true;
-  };
-}
--- nginx/www.A.nix
+++
@@ -1,4 +0,0 @@
-{
-  enableACME = true;
-  addSSL = true;
-}
